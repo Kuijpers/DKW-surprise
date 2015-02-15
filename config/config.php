d77052e3f9f7ee06303a7e5a52a61d79b941c9d3
Kuijpers <darkknightworks@gmail.com>
2015-02-15 13:37:45 +0100

    Initial files

---
+++ config/config.php
@@ -0,0 +1,50 @@
+<?php
+
+// Configuratie gedeelte database verbinding
+	
+	/*
+	$dbhost			=	"localhost";
+	$dbuser			=	"deb44110_surp";
+	$dbpass			=	"4765bm22";
+	$dbname			=	"deb44110_surprise";
+	*/
+	
+	$dbhost			=	"localhost";
+	$dbuser			=	"root";
+	$dbpass			=	"";
+	$dbname			=	"deb44110_surprise";
+	
+	
+	
+// Indeling van de website
+	
+	# Plaatsing van het menu
+	
+	$menu_set		=	"1";		//	Topmenu 		=	1
+									//	Menu rechts 	=	2
+									//	Menu links		=	3
+									//	Menu Alternate	=	4 !! Bij deze instelling overrule je alle menu instelling en maak je er zelf een css voor.
+	
+	# Taal instellingen
+							
+	$main_lang		=	"en";		//	Hierbij gebruiken we de iso waardes
+	
+	$lang_selection	=	"";			
+	
+	$lang_sel_type	=	"2";		//	Drop down select menu	=	1
+									//	Vlag selectie			=	2
+	
+	# Date / time
+	
+	$show_date		=	"1";		//	Yes	=	1
+									//	No	=	0
+	
+	# Footer informatie
+	
+	$copyright		=	"Dennis Kuijpers Divigo Design &copy; 2010";
+
+
+
+
+
+?>
